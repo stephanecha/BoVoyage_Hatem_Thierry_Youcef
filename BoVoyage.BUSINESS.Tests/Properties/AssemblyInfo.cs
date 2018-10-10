68bc4d0299a3f813bb6dcc0f58ebbe440e08ba7c
YoosZ <so_yoos@msn.com>
2018-10-10 11:20:26 +0200

    Initialisation du projet avec les packages nugger nécessaires et références entre les différentes couches.

---
+++ BoVoyage.BUSINESS.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BoVoyage.BUSINESS.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BoVoyage.BUSINESS.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ebffb3cd-cb53-4826-a82e-d9ba4064bef5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
