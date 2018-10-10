6434bfe3d24f1d5d1ad7e524f212d1a559975247
YoosZ <so_yoos@msn.com>
2018-10-10 19:30:35 +0200

    Ajout d'un validator AgeOver

---
+++ BoVoyage.WEB/Validators/AgeOver.cs
@@ -0,0 +1,38 @@
+﻿using BoVoyage.COMMON.Tools;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BoVoyage.WEB.Validators
+{
+	[AttributeUsage(AttributeTargets.Property)]
+	public class AgeOver : ValidationAttribute
+	{
+		public int MinimumAge { get; private set; }
+
+		public AgeOver(int minimumAge)
+		{
+			this.MinimumAge = minimumAge;
+		}
+
+		public override bool IsValid(object value)
+		{
+			if (value != null)
+			{
+				if (value is DateTime valueDateTime)
+				{
+					return DateTime.Now.AddYears(-this.MinimumAge) >= valueDateTime;
+				}
+				else
+				{
+					throw new ArgumentException(MessageType.MustBeDateTime);
+				}
+			}
+			return false;
+		}
+
+		public override string FormatErrorMessage(string name)
+		{
+			return string.Format(this.ErrorMessage, name, this.MinimumAge.ToString());
+		}
+	}
+}
