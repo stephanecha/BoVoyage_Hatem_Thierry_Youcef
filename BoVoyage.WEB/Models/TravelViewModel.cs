1516929ee241564d60373006d3245206fa2a1eec
YoosZ <so_yoos@msn.com>
2018-10-10 18:58:53 +0200

    Modification du Travel ViewModel

--- BoVoyage.WEB/Models/TravelViewModel.cs
+++ BoVoyage.WEB/Models/TravelViewModel.cs
@@ -1,28 +1,50 @@
-﻿using BoVoyage.DAL.Entites;
+﻿using BoVoyage.COMMON.Tools;
+using BoVoyage.DAL.Entites;
 using BoVoyage.WEB.Models.Base;
+using BoVoyage.WEB.Validators;
+using BoVoyage.WEB.Validators.Enum;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace BoVoyage.WEB.Models
 {
-    public sealed class TravelViewModel : BaseModelViewModel
-    {
-        public int Id { get; set; }
+	public sealed class TravelViewModel : BaseModelViewModel
+	{
+		[Required(ErrorMessage = MessageType.RequiredField)]
+		[Display(Name = "Date aller")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+		[DateBeforeAfterToday(CompareDateCase.AFTER, ErrorMessage = "Le champ {0} doit être après aujourd'hui.")]
+		//TODO: Unique DepartureDate, ReturnDate, TravelAgencyID and DestinationID and PricePerPerson
+		public DateTime DepartureDate { get; set; }
 
-        [Required]
-        public DateTime DepartureDate { get; set; }
+		[Required(ErrorMessage = MessageType.RequiredField)]
+		[Display(Name = "Date retour")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+		[DateComparaison(CompareDateCase.AFTER, "DepartureDate", ErrorMessage = "Le champ {0} doit être après {1}")]
+		public DateTime ReturnDate { get; set; }
 
-        [Required]
-        public DateTime ReturnDate { get; set; }
+		[Required(ErrorMessage = MessageType.RequiredField)]
+		[Display(Name = "Places disponibles")]
+		[IntDecimalOverZero(ErrorMessage = "Le champ {0} doit être supérieur ou égal à 0.")]
+		public int AvailablePlaces { get; set; }
 
-        [Required]
-        public int AvailablePlaces { get; set; }
+		[Required(ErrorMessage = MessageType.RequiredField)]
+		[Display(Name = "Prix par personne")]
+		[IntDecimalOverZero(ErrorMessage = MessageType.MustBeOverZero)]
+		[DataType(DataType.Currency)]
+		public decimal PricePerPerson { get; set; }
 
-        [Required]
-        public decimal PricePerPerson { get; set; }
+		[Required(ErrorMessage = MessageType.RequiredField)]
+		[Display(Name = "Agence de voyage")]
+		public int TravelAgencyID { get; set; }
 
-        //[Display]
-        public ICollection<BookingFile> BookingFiles { get; set; }
-    }
+		[Required(ErrorMessage = MessageType.RequiredField)]
+		[Display(Name = "Destination")]
+		public int DestinationID { get; set; }
+
+		public ICollection<BookingFile> BookingFiles { get; set; }
+	}
 }
