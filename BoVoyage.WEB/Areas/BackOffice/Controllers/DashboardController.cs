ebc5d422c6ce5ae8514fcde6d069c9c2d3cab3ab
Hatem <hatem.saturi@gmail.com>
2018-10-10 21:17:38 +0200

    Mise en place de l'affichage des prochains voyages sous 5 jours

--- BoVoyage.WEB/Areas/BackOffice/Controllers/DashboardController.cs
+++ BoVoyage.WEB/Areas/BackOffice/Controllers/DashboardController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BoVoyage.BUSINESS.Services;
+using BoVoyage.DAL.Data;
+using BoVoyage.DAL.Entites;
+using BoVoyage.WEB.Models;
+
 
 namespace BoVoyage.WEB.Areas.BackOffice.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly ServiceTravel serviceTravel;
+        public DashboardController()
+        {
+            this.serviceTravel = new ServiceTravel(new DbDataTravel());
+        }
         // GET: BackOffice/Dashboard
         public ActionResult Index()
         {
-            return View();
+
+            var travel = serviceTravel.GetAllTravels().Where(x=>x.DepartureDate<(DateTime.Today.AddDays(15))).OrderBy(x=>x.DepartureDate);
+
+            return View(travel);
         }
     }
 }
