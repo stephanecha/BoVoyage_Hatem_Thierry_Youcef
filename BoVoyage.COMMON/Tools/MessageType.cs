cf25e7c4cdff907cea8739a14b174dffa39557bb
YoosZ <so_yoos@msn.com>
2018-10-10 18:30:23 +0200

    Création d'une classe static regroupant tout les messages utilisés.

---
+++ BoVoyage.COMMON/Tools/MessageType.cs
@@ -0,0 +1,13 @@
+﻿namespace BoVoyage.COMMON.Tools
+{
+	public static class MessageType
+	{
+		public const string RequiredField = "Le champ {0} est obligatoire.";
+		public const string StringLengthField = "Le champ {0} doit contenir {1} caractères max.";
+		public const string OneFieldUnique = "Il existe déjà un champ {0} avec la valeur {1}.";
+
+		public const string MustBeString = "Le type doit être une string.";
+		public const string MustBeDateTime = "Le type doit être un DateTime.";
+		public const string MustBeintOrDecimal = "Le type doit être un int ou decimal.";
+	}
+}
