a716b5c20a4761a56d47b0279e6b4ecf149ff84e
YoosZ <so_yoos@msn.com>
2018-10-10 18:32:46 +0200

    Ajout d'un validator pour rechercher grace au dbContext un même champ Name pour la table Agencies

---
+++ BoVoyage.DAL/Validators/UniqueTravelAgencyName.cs
@@ -0,0 +1,36 @@
+﻿using BoVoyage.COMMON.Tools;
+using BoVoyage.DAL.Context;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace BoVoyage.DAL.Validators
+{
+	[AttributeUsage(AttributeTargets.Property)]
+	public class UniqueTravelAgencyName : ValidationAttribute
+	{
+		public override bool IsValid(object value)
+		{
+			if (value != null)
+			{
+				if (value is string)
+				{
+					using (DataContext db = new DataContext())
+					{
+						return !db.TravelAgencies.Any(x => x.Name == value.ToString());
+					}
+				}
+				else
+				{
+					throw new ArgumentException(MessageType.OneFieldUnique);
+				}
+			}
+			return false;
+		}
+
+		public override string FormatErrorMessage(string name)
+		{
+			return string.Format(this.ErrorMessage, name);
+		}
+	}
+}
