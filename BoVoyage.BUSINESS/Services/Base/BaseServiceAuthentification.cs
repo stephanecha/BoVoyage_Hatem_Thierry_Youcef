65af644c38cc09eaea8917ec9b34bc1d85d5c9a5
YoosZ <so_yoos@msn.com>
2018-10-10 14:04:45 +0200

    Ajout d'une classe abstract pour intégrer l'interface d'accés aux Authentifications aux Services Customer et SalesManager

---
+++ BoVoyage.BUSINESS/Services/Base/BaseServiceAuthentification.cs
@@ -0,0 +1,14 @@
+﻿using BoVoyage.DAL.Data.Interface;
+
+namespace BoVoyage.BUSINESS.Services.Base
+{
+	public abstract class BaseServiceAuthentification
+	{
+		private readonly IDataAuthentification dataAuthentification;
+
+		protected BaseServiceAuthentification(IDataAuthentification dataAuthentification)
+		{
+			this.dataAuthentification = dataAuthentification;
+		}
+	}
+}
