4255198505d057c5d0b571c25c316f49b467dab7
YoosZ <so_yoos@msn.com>
2018-10-11 23:55:21 +0200

    Création de la static class TransformModelDestination pour les transformations de Model à View

---
+++ BoVoyage.WEB/Tools/TransformModelDestination.cs
@@ -0,0 +1,26 @@
+﻿using BoVoyage.DAL.Entites;
+using BoVoyage.WEB.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BoVoyage.WEB.Tools
+{
+	public static class TransformModelDestination
+	{
+		public static List<DestinationViewModel> DestinationToModelView(IEnumerable<Destination> listDestinations)
+		{
+			List<DestinationViewModel> destinationToModelView = new List<DestinationViewModel>(listDestinations.Select(x =>
+				new DestinationViewModel()
+				{
+					ID = x.ID,
+					Description = x.Description,
+					City = x.City,
+					Area = x.Area,
+					Continent = x.Continent,
+					Country = x.Country,
+					Travels = x.Travels
+				}).ToList());
+			return destinationToModelView;
+		}
+	}
+}
