b084a220a0df11fc17f450b9cf6a1e38be241d68
YoosZ <so_yoos@msn.com>
2018-10-11 23:46:12 +0200

    Modif mineur

--- BoVoyage.WEB/Areas/BackOffice/Controllers/TravelsController.cs
+++ BoVoyage.WEB/Areas/BackOffice/Controllers/TravelsController.cs
@@ -1,103 +1,98 @@
 ﻿using BoVoyage.BUSINESS.Services;
 using BoVoyage.DAL.Data;
-using BoVoyage.WEB.Models;
 using BoVoyage.WEB.Tools;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BoVoyage.WEB.Areas.BackOffice.Controllers
 {
-    public class TravelsController : Controller
-    {
-        private readonly ServiceTravel serviceTravel;
+	public class TravelsController : Controller
+	{
+		private readonly ServiceTravel serviceTravel;
 
-        public TravelsController()
-        {
-            this.serviceTravel = new ServiceTravel(new DbDataTravel());
-        }
+		public TravelsController()
+		{
+			this.serviceTravel = new ServiceTravel(new DbDataTravel());
+		}
 
-        // GET: BackOffice/Travels
-        public ActionResult Index()
-        {
-            var travel = serviceTravel.GetAllTravelsWithDestination();
+		// GET: BackOffice/Travels
+		public ActionResult Index()
+		{
+			var travel = serviceTravel.GetAllTravelsWithDestination();
 
-            var travelViewModel = TransformModelToModelView.TravelToSearchModelView(travel);
-            return View(travelViewModel);
-        }
+			var travelViewModel = TransformModelTravel.TravelToSearchModelView(travel);
+			return View(travelViewModel);
+		}
 
-        // GET: BackOffice/Travels/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+		// GET: BackOffice/Travels/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
 
-        // GET: BackOffice/Travels/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		// GET: BackOffice/Travels/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: BackOffice/Travels/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+		// POST: BackOffice/Travels/Create
+		[HttpPost]
+		public ActionResult Create(FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add insert logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-        // GET: BackOffice/Travels/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+		// GET: BackOffice/Travels/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
-        // POST: BackOffice/Travels/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+		// POST: BackOffice/Travels/Edit/5
+		[HttpPost]
+		public ActionResult Edit(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add update logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-        // GET: BackOffice/Travels/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+		// GET: BackOffice/Travels/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
 
-        // POST: BackOffice/Travels/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+		// POST: BackOffice/Travels/Delete/5
+		[HttpPost]
+		public ActionResult Delete(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add delete logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
