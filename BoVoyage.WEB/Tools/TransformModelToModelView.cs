2e811f9e78cec93b948e43518fc41968b31a6d5a
HatemSat <hatem.saturi@gmail.com>
2018-10-11 16:55:37 +0200

    Correction de la fusion au niveau du transformviewmodel

--- BoVoyage.WEB/Tools/TransformModelToModelView.cs
+++ BoVoyage.WEB/Tools/TransformModelToModelView.cs
@@ -44,23 +44,6 @@ namespace BoVoyage.WEB.Tools
 
 			return insurancetype;
 		}
-	}
-    public static class TransformModelToModelView
-    {
-        public static List<InsuranceViewModel> InsuranceToModelView(IEnumerable<Insurance> listAssurances)
-        {
-            List<InsuranceViewModel> assuranceViewModel = new List<InsuranceViewModel>(listAssurances.Select(x =>
-                new InsuranceViewModel()
-                {
-                    ID = x.ID,
-                    Description = x.Description,
-                    Price = x.Price,
-                    InsuranceTypeID = x.InsuranceTypeID,
-                    InsuranceType = x.InsuranceType
-                }).ToList());
-            return assuranceViewModel;
-        }
-
         public static List<TravelAgencyViewModel> TravelAgencyToModelView(IEnumerable<TravelAgency> listAgences)
         {
             List<TravelAgencyViewModel> agencesViewModel = new List<TravelAgencyViewModel>(listAgences.Select(x =>
@@ -69,9 +52,10 @@ namespace BoVoyage.WEB.Tools
                     ID = x.ID,
                     Name = x.Name,
                     Travels = x.Travels
-                   
+
                 }).ToList());
             return agencesViewModel;
         }
     }
+   
 }
