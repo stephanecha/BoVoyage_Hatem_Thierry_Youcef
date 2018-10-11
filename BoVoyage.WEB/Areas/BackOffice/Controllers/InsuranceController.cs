c6335ad991363b5750cd4b8d9428296557f47f9d
YoosZ <so_yoos@msn.com>
2018-10-11 23:47:29 +0200

    controller des Insurances complet et sécurisé

--- BoVoyage.WEB/Areas/BackOffice/Controllers/InsuranceController.cs
+++ BoVoyage.WEB/Areas/BackOffice/Controllers/InsuranceController.cs
@@ -1,124 +1,157 @@
 ﻿using BoVoyage.BUSINESS.Services;
 using BoVoyage.DAL.Data;
 using BoVoyage.DAL.Entites;
+using BoVoyage.WEB.Areas.BackOffice.Controllers.Base;
 using BoVoyage.WEB.Models;
 using BoVoyage.WEB.Tools;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BoVoyage.WEB.Areas.BackOffice.Controllers
 {
-    public class InsuranceController : Controller
-    {
-        private readonly ServiceInsurance serviceInsurance;
-        private readonly ServiceInsuranceType serviceInsuranceType;
+	public class InsuranceController : BaseController
+	{
+		private readonly ServiceInsurance serviceInsurance;
+		private readonly ServiceInsuranceType serviceInsuranceType;
 
-        public InsuranceController()
-        {
-            this.serviceInsurance = new ServiceInsurance(new DbDataInsurance());
-            this.serviceInsuranceType = new ServiceInsuranceType(new DbDataInsuranceType());
+		public InsuranceController()
+		{
+			this.serviceInsurance = new ServiceInsurance(new DbDataInsurance());
+			this.serviceInsuranceType = new ServiceInsuranceType(new DbDataInsuranceType());
+		}
 
-        }
-        // GET: BackOffice/Insurance
-        public ActionResult Index()
-        {
+		// GET: BackOffice/Insurance
+		public ActionResult Index()
+		{
+			var assurance = serviceInsurance.GetAllInsurancesWithTypesAndBookingFilesIncluded();
+			var assuranceViewModel = TransformModelInsurance.InsuranceToModelView(assurance);
 
-            var assurance = serviceInsurance.GetAllInsurancesWithTypesIncluded();
+			return View(assuranceViewModel);
+		}
 
-            var assuranceViewModel = TransformModelToModelView.InsuranceToModelView(assurance);
+		// GET: BackOffice/Insurance/Create
+		public ActionResult Create()
+		{
+			SelectList insuranceTypesValues = new SelectList(serviceInsuranceType.GetAllInsuranceTypes(), "ID", "Type");
+			ViewBag.Types = insuranceTypesValues;
 
-            return View(assuranceViewModel);
-        }
+			return View();
+		}
 
-        // GET: BackOffice/Insurance/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+		// POST: BackOffice/Insurance/Create
+		[HttpPost]
+		public ActionResult Create([Bind(Exclude = "ID")] InsuranceViewModel insuranceViewModel, int InsuranceTypeID)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					Insurance insurance = TransformModelInsurance.InsuranceModelViewToModel(insuranceViewModel);
+					this.serviceInsurance.AddInsurance(insurance);
+					Display("Le nouveau type d'assurance a bien été enregistré !");
+					return RedirectToAction("Index");
+				}
+				else
+				{
+					SelectList insuranceTypesValues = new SelectList(this.serviceInsuranceType.GetAllInsuranceTypes(), "ID", "Type",
+						insuranceViewModel.InsuranceType);
+					ViewBag.Types = insuranceTypesValues;
+					return View(insuranceViewModel);
+				}
+				// TODO: Add insert logic here
+			}
+			catch
+			{
+				Display("Erreur !", MessageType.ERROR);
+				SelectList insuranceTypesValues = new SelectList(this.serviceInsuranceType.GetAllInsuranceTypes(), "ID", "Type");
+				ViewBag.Types = insuranceTypesValues;
+				return View();
+			}
+		}
 
-        // GET: BackOffice/Insurance/Create
-        public ActionResult Create()
-        {
-           
-            SelectList InsuranceTypesValues = new SelectList(serviceInsuranceType.GetAllInsuranceTypes(), "ID", "Type");
-            ViewBag.types = InsuranceTypesValues;
-            
-            return View();
-        }
+		// GET: BackOffice/Insurance/Edit/5
+		public ActionResult Edit(int id)
+		{
+			Insurance insurance = this.serviceInsurance.GetInsurance(id);
+			if (insurance == null)
+				return HttpNotFound();
+			InsuranceViewModel insuranceViewModel = TransformModelInsurance.InsuranceToModelView(insurance);
 
-        // POST: BackOffice/Insurance/Create
-        [HttpPost]
-        public ActionResult Create(InsuranceViewModel insuranceViewModel, int typeID)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    var insurance = new Insurance() {
-                        Price = insuranceViewModel.Price,
-                        Description = insuranceViewModel.Description,
-                        InsuranceTypeID = typeID
-                    };
-                    this.serviceInsurance.AddInsurance(insurance);
-                    return RedirectToAction("Index");
+			SelectList insuranceTypesValues = new SelectList(this.serviceInsuranceType.GetAllInsuranceTypes(), "ID", "Type",
+				this.serviceInsuranceType.GetInsuranceType(insurance.InsuranceTypeID));
+			ViewBag.Types = insuranceTypesValues;
 
-                }
-                else
-                    return View(insuranceViewModel);
-                // TODO: Add insert logic here
+			return View(insuranceViewModel);
+		}
 
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		// POST: BackOffice/Insurance/Edit/5
+		[HttpPost]
+		public ActionResult Edit(int id, InsuranceViewModel insuranceViewModel, int InsuranceTypeID)
+		{
+			try
+			{
+				if (insuranceViewModel == null)
+					return HttpNotFound();
+				if (id != insuranceViewModel.ID)
+					return HttpNotFound();
 
-        // GET: BackOffice/Insurance/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+				if (ModelState.IsValid)
+				{
+					Insurance insurance = TransformModelInsurance.InsuranceModelViewToModel(insuranceViewModel);
+					this.serviceInsurance.UpdateInsurance(insurance);
+					Display("Le type d'assurance a bien été modifié !");
+					return RedirectToAction("Index");
+				}
+				else
+				{
+					SelectList insuranceTypesValues = new SelectList(this.serviceInsuranceType.GetAllInsuranceTypes(), "ID", "Type",
+						insuranceViewModel.InsuranceTypeID);
+					ViewBag.Types = insuranceTypesValues;
+					return View(insuranceViewModel);
+				}
+				// TODO: Add update logic here
+			}
+			catch
+			{
+				Display("Erreur !", MessageType.ERROR);
+				SelectList insuranceTypesValues = new SelectList(this.serviceInsuranceType.GetAllInsuranceTypes(), "ID", "Type",
+					insuranceViewModel.InsuranceTypeID);
+				ViewBag.Types = insuranceTypesValues;
+				return View();
+			}
+		}
 
-        // POST: BackOffice/Insurance/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+		// GET: BackOffice/Insurance/Delete/5
+		public ActionResult Delete(int id)
+		{
+			Insurance insurance = this.serviceInsurance.GetInsuranceWithTypesIncluded(id);
+			if (insurance == null)
+				return HttpNotFound();
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+			InsuranceViewModel insuranceViewModel = TransformModelInsurance.InsuranceToModelView(insurance);
+			return View(insuranceViewModel);
+		}
 
-        // GET: BackOffice/Insurance/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+		// POST: BackOffice/Insurance/Delete/5
+		[HttpPost]
+		public ActionResult Delete(int id, InsuranceViewModel insuranceViewModel)
+		{
+			try
+			{
+				if (insuranceViewModel == null)
+					return HttpNotFound();
+				if (id != insuranceViewModel.ID)
+					return HttpNotFound();
 
-        // POST: BackOffice/Insurance/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+				// TODO: Add delete logic here
+				this.serviceInsurance.DeleteInsurance(id);
+				Display("L'assurance a bien été supprimé !");
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				Display("Erreur !", MessageType.ERROR);
+				return RedirectToAction("Index");
+			}
+		}
+	}
 }
