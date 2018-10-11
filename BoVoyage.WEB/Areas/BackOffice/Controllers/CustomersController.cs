4b677a6c8316930933c3e69b4e5ce66dff89c815
YoosZ <so_yoos@msn.com>
2018-10-11 16:44:57 +0200

    Supression des erreurs empéchant la compilation

--- BoVoyage.WEB/Areas/BackOffice/Controllers/CustomersController.cs
+++ BoVoyage.WEB/Areas/BackOffice/Controllers/CustomersController.cs
@@ -1,114 +1,102 @@
-﻿using BoVoyage.BUSINESS.Services;
+﻿r"using BoVoyage.BUSINESS.Services;
+
 using BoVoyage.DAL.Data;
-using BoVoyage.DAL.Entites;
 using BoVoyage.WEB.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BoVoyage.WEB.Areas.BackOffice.Controllers
 {
-    public class CustomersController : Controller
-    {
-        private readonly ServiceCustomer serviceCustomer;
+	public class CustomersController : Controller
+	{
+		private readonly ServiceCustomer serviceCustomer;
 
-        public CustomersController()
-        {
-            this.serviceCustomer = new ServiceCustomer(new DbDataAuthentification(), new DbDataCustomer());
-        }
+		public CustomersController()
+		{
+			this.serviceCustomer = new ServiceCustomer(new DbDataAuthentification(), new DbDataCustomer());
+		}
 
-        // GET: BackOffice/Customers
-        public ActionResult Index()
-        {
-            var customerIndex = serviceCustomer.GetAllCustomers();
-            return View(customerIndex);
-        }
+		// GET: BackOffice/Customers
+		public ActionResult Index()
+		{
+			var customerIndex = serviceCustomer.GetAllCustomers();
+			return View(customerIndex);
+		}
 
-        // GET: BackOffice/Customers/Details/5
-        public ActionResult Details(int id)
-        {
-            var customerDetails = serviceCustomer.GetCustomer(id);
-            return View(customerDetails);
-        }
+		// GET: BackOffice/Customers/Details/5
+		public ActionResult Details(int id)
+		{
+			var customerDetails = serviceCustomer.GetCustomer(id);
+			return View(customerDetails);
+		}
 
-        // GET: BackOffice/Customers/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		// GET: BackOffice/Customers/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: BackOffice/Customers/Create
-        [HttpPost]
-        public ActionResult Create(CustomerViewModel customer)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    var customerCreatePost = new CustomerViewModel();
-                    {
-                        BirthDate = customerViewModel.BirthDate,
-                        BookingFiles = customerViewModel.BookingFiles
-                    }
-                // TODO revoir le customer car ne marche pour le moment
-                    this.serviceCustomer.AddCustomer(customer);
-                    return RedirectToAction("Index");
-                }
+		// POST: BackOffice/Customers/Create
+		[HttpPost]
+		public ActionResult Create(CustomerViewModel customer)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					return RedirectToAction("Index");
+				}
+				else
+					return View(ModelState);
+				// TODO: Add insert logic here
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-                else
-                    return View(ModelState);
-                // TODO: Add insert logic here
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		// GET: BackOffice/Customers/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
-        // GET: BackOffice/Customers/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+		// POST: BackOffice/Customers/Edit/5
+		[HttpPost]
+		public ActionResult Edit(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add update logic here
 
-        // POST: BackOffice/Customers/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		// GET: BackOffice/Customers/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
 
-        // GET: BackOffice/Customers/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+		// POST: BackOffice/Customers/Delete/5
+		[HttpPost]
+		public ActionResult Delete(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add delete logic here
 
-        // POST: BackOffice/Customers/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
