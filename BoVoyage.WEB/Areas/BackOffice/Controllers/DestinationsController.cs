82cc18cd64637b3c09c1543e603aefe55744e70b
Ben <x>
2018-10-11 13:34:46 +0200

    Creation : Controller Destination

--- BoVoyage.WEB/Areas/BackOffice/Controllers/DestinationsController.cs
+++ BoVoyage.WEB/Areas/BackOffice/Controllers/DestinationsController.cs
@@ -1,5 +1,6 @@
 ﻿using BoVoyage.BUSINESS.Services;
 using BoVoyage.DAL.Data;
+using BoVoyage.DAL.Entites;
 using BoVoyage.WEB.Models;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ namespace BoVoyage.WEB.Areas.BackOffice.Controllers
     public class DestinationsController : Controller
     {
         private readonly ServiceDestination serviceDestination;
+
         public DestinationsController()
         {
             this.serviceDestination = new ServiceDestination(new DbDataDestination());
@@ -19,33 +21,46 @@ namespace BoVoyage.WEB.Areas.BackOffice.Controllers
         // GET: BackOffice/Destinations
         public ActionResult Index()
         {
-            var destination = serviceDestination.GetAllDestinations();
-            return View(destination);
+            var destinationIndex = serviceDestination.GetAllDestinations();
+            return View(destinationIndex);
         }
 
         // GET: BackOffice/Destinations/Details/5
         public ActionResult Details(int id)
         {
-            //int destination = serviceDestination.?
-            return View(id);
+            var destinationDetails = serviceDestination.GetDestination(id);
+            return View(destinationDetails);
         }
 
         // GET: BackOffice/Destinations/Create
         public ActionResult Create()
         {
-            //var destination = serviceDestination.AddDestination();
             return View();
         }
 
         // POST: BackOffice/Destinations/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(DestinationViewModel destinationViewModel)
         {
             try
             {
+                if (ModelState.IsValid)
+                {
+                    var destinationCreatePost = new Destination() {
+                        Continent = destinationViewModel.Continent,
+                        Country = destinationViewModel.Country,
+                        Area = destinationViewModel.Area,
+                        City = destinationViewModel.City,
+                        Description = destinationViewModel.Description 
+                        };
+                    this.serviceDestination.AddDestination(destinationCreatePost);
+                    return RedirectToAction("Index");
+
+                }
+                else
+                    return View(destinationViewModel);
                 // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
             }
             catch
             {
@@ -56,6 +71,7 @@ namespace BoVoyage.WEB.Areas.BackOffice.Controllers
         // GET: BackOffice/Destinations/Edit/5
         public ActionResult Edit(int id)
         {
+            //var destinationEdit = serviceDestination.UpdateDestination(id);
             return View();
         }
 
@@ -65,6 +81,7 @@ namespace BoVoyage.WEB.Areas.BackOffice.Controllers
         {
             try
             {
+                //var destinationEditCollection = serviceDestination.UpdateDestination(id,collection);
                 // TODO: Add update logic here
 
                 return RedirectToAction("Index");
