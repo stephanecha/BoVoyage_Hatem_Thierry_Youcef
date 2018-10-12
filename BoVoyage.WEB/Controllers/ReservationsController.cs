189045f1789272352adea28a7f0619d8db5aae94
YoosZ <so_yoos@msn.com>
2018-10-12 19:28:48 +0200

    Controller reservation, avec vue et modelView

---
+++ BoVoyage.WEB/Controllers/ReservationsController.cs
@@ -0,0 +1,79 @@
+﻿using System.Web.Mvc;
+
+namespace BoVoyage.WEB.Controllers
+{
+	public class ReservationsController : Controller
+	{
+		// GET: Reservations/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: Reservations/Create/6/8
+		public ActionResult Create(int idCustomer, int idTravel)
+		{
+			return View();
+		}
+
+		// POST: Reservations/Create
+		[HttpPost]
+		public ActionResult Create(FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add insert logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: Reservations/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: Reservations/Edit/5
+		[HttpPost]
+		public ActionResult Edit(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add update logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: Reservations/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: Reservations/Delete/5
+		[HttpPost]
+		public ActionResult Delete(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add delete logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
---
+++ BoVoyage.WEB/Models/ReservationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BoVoyage.WEB.Models
+{
+	public class ReservationViewModel
+	{
+		public BookingFileViewModel BookingFileViewModel { get; set; }
+
+		public CustomerViewModel CustomerViewModel { get; set; }
+
+		public TravelViewModel TravelViewModel { get; set; }
+
+		public IEnumerable<TravelerViewModel> TravelersList { get; set; }
+	}
+}
