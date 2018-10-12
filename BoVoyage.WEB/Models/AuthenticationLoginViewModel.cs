d451b000f74c4b33cf4ef920400b6bc707d3220e
YoosZ <so_yoos@msn.com>
2018-10-12 03:54:40 +0200

    Création d'un modèleView d'authentification

---
+++ BoVoyage.WEB/Models/AuthenticationLoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoVoyage.WEB.Models
+{
+	public class AuthenticationLoginViewModel
+	{
+		[Display(Name = "Login")]
+		[Required(ErrorMessage = "Le champ {0} est obligatoire.")]
+		public string Mail { get; set; }
+
+		[Display(Name = "Mot de passe")]
+		[Required(ErrorMessage = "Le champ {0} est obligatoire.")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
