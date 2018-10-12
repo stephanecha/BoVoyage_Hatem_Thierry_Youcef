ee2aa01863bbf14eabd5463d1a5f18998a1651b2
YoosZ <so_yoos@msn.com>
2018-10-12 03:54:16 +0200

    création du SalesManager viewModel

---
+++ BoVoyage.WEB/Models/SalesManagerViewModel.cs
@@ -0,0 +1,27 @@
+﻿using BoVoyage.WEB.Models.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BoVoyage.WEB.Models
+{
+	public class SalesManagerViewModel : BasePersonViewModel
+	{
+		[Required]
+		public DateTime BirthDate { get; set; }
+
+		public int Age
+		{
+			get { return DateTime.Today.Year - BirthDate.Year; }
+		}
+
+		[Required]
+		[StringLength(60)]
+		public string Email { get; set; }
+
+		[Required]
+		[StringLength(250)]
+		public string Password { get; set; }
+
+		public string ConfirmedPassword { get; set; }
+	}
+}
