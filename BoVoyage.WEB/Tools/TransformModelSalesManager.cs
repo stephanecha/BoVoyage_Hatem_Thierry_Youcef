d2bff3e1e94ac98244ff06bbc3a95ab5fa847e2b
YoosZ <so_yoos@msn.com>
2018-10-12 03:53:34 +0200

    Création de la classe pour transformer les View en Model SalesManager et vice versa

---
+++ BoVoyage.WEB/Tools/TransformModelSalesManager.cs
@@ -0,0 +1,25 @@
+﻿using BoVoyage.DAL.Entites;
+using BoVoyage.WEB.Models;
+
+namespace BoVoyage.WEB.Tools
+{
+	public static class TransformModelSalesManager
+	{
+		public static SalesManagerViewModel SalesManagerToModelView(SalesManager salesManager)
+		{
+			SalesManagerViewModel salesManagerViewModel = new SalesManagerViewModel()
+			{
+				ID = salesManager.ID,
+				Civility = salesManager.Civility,
+				FirstName = salesManager.FirstName,
+				LastName = salesManager.LastName,
+				Address = salesManager.Address,
+				PhoneNumber = salesManager.PhoneNumber,
+				BirthDate = salesManager.BirthDate,
+				Email = salesManager.Authentification.Email
+			};
+
+			return salesManagerViewModel;
+		}
+	}
+}
