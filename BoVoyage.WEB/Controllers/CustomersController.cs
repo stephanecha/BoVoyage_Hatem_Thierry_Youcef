d6418cc45bd7cec3c2a50eb0e4772605e00a33a7
YoosZ <so_yoos@msn.com>
2018-10-13 06:24:05 +0200

    Customer Account gestion

--- BoVoyage.WEB/Controllers/CustomersController.cs
+++ BoVoyage.WEB/Controllers/CustomersController.cs
@@ -2,7 +2,6 @@
 using BoVoyage.COMMON.Extensions;
 using BoVoyage.DAL.Data;
 using BoVoyage.WEB.Controllers.Base;
-using BoVoyage.DAL.Entites;
 using BoVoyage.WEB.Filters;
 using BoVoyage.WEB.Models;
 using BoVoyage.WEB.Tools;
@@ -13,24 +12,13 @@ namespace BoVoyage.WEB.Controllers
 	public class CustomersController : BaseController
 	{
 		private readonly ServiceCustomer serviceCustomer;
+		private readonly ServiceBookingFile serviceBookingFile;
 
-        public CustomersController()
-        {
-            this.serviceCustomer = new ServiceCustomer(new DbDataAuthentification(), new DbDataCustomer());
-        }
-        // GET: Customers
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Customers/Details/5
-        [Authentication(Type = "CUSTOMER")]
-        public ActionResult Details()
-        {
-            CustomerViewModel customerViewModel = (CustomerViewModel)Session["CUSTOMER"];
-            return View(customerViewModel);
-        }
+		public CustomersController()
+		{
+			this.serviceCustomer = new ServiceCustomer(new DbDataAuthentification(), new DbDataCustomer());
+			this.serviceBookingFile = new ServiceBookingFile(new DbDataBookingFile());
+		}
 
 		// GET: Customers/Create
 		public ActionResult Subscribe()
@@ -40,13 +28,13 @@ namespace BoVoyage.WEB.Controllers
 
 		// POST: Customers/Create
 		[HttpPost]
-		public ActionResult Subscribe([Bind(Exclude = "ID, AuthentificationID")] CustomerViewModel customerViewModel)
+		public ActionResult Subscribe([Bind(Exclude = "ID, AuthentificationID")] CustomerViewModel model)
 		{
 			try
 			{
 				if (ModelState.IsValid)
 				{
-					var customer = TransformModelCustomer.CustomerModelViewToModel(customerViewModel);
+					var customer = TransformModelCustomer.CustomerModelViewToModel(model);
 					customer.Authentification.Password = customer.Authentification.Password.GenerateSHA512String();
 
 					this.serviceCustomer.AddCustomer(customer.Authentification, customer);
@@ -56,7 +44,7 @@ namespace BoVoyage.WEB.Controllers
 				}
 				else
 				{
-					return View(customerViewModel);
+					return View(model);
 				}
 				// TODO: Add insert logic here
 			}
@@ -67,48 +55,74 @@ namespace BoVoyage.WEB.Controllers
 			}
 		}
 
+		// GET: Customers/Details/5
+		[Authentication(Type = "CUSTOMER")]
+		public ActionResult Details()
+		{
+			CustomerViewModel customerViewModel = (CustomerViewModel)Session["CUSTOMER"];
+			return View(customerViewModel);
+		}
+
 		// GET: Customers/Edit/5
+		[Authentication(Type = "CUSTOMER")]
 		public ActionResult Edit(int id)
 		{
-			return View();
+			var customer = (CustomerViewModel)Session["CUSTOMER"];
+			if (customer.ID != id)
+				return HttpNotFound();
+			CustomerViewModel customerViewModel = customer;
+			return View(customerViewModel);
 		}
 
 		// POST: Customers/Edit/5
 		[HttpPost]
-		public ActionResult Edit(int id, FormCollection collection)
+		[Authentication(Type = "CUSTOMER")]
+		public ActionResult Edit(int id, CustomerViewModel model)
 		{
 			try
 			{
+				if (model == null || id != model.ID)
+					return HttpNotFound();
 				// TODO: Add update logic here
 
-				return RedirectToAction("Index");
+				if (ModelState.IsValid)
+				{
+					var customer = this.serviceCustomer.GetCustomerWithAuthentificationInclude(id);
+					customer.Civility = model.Civility;
+					customer.FirstName = model.FirstName;
+					customer.LastName = model.LastName;
+					customer.BirthDate = model.BirthDate;
+					customer.Address = model.Address;
+					customer.PhoneNumber = model.PhoneNumber;
+					customer.Authentification.Password = model.Password;
+					customer.Authentification.Password = model.Email;
+
+					this.serviceCustomer.UpdateCustomer(customer);
+
+					CustomerViewModel customerViewModel = TransformModelCustomer.CustomerToModelView(customer);
+					Session["CUSTOMER"] = customerViewModel;
+					Display("Les changements sont bien enregistrés");
+					return RedirectToAction("details");
+				}
+				else
+				{
+					return View(model);
+				}
 			}
 			catch
 			{
+				Display("Erreur !", MessageType.ERROR);
 				return View();
 			}
 		}
 
-		// GET: Customers/Delete/5
-		public ActionResult Delete(int id)
+		// GET: Customers/Create
+		[Authentication(Type = "CUSTOMER")]
+		public ActionResult Reservation()
 		{
+			CustomerViewModel customerViewModel = (CustomerViewModel)Session["CUSTOMER"];
+			ViewBag.List = this.serviceBookingFile.GetAllBookingFilesWithTravelsAndDestinationsIncluded(customerViewModel.ID);
 			return View();
-		}
-
-		// POST: Customers/Delete/5
-		[HttpPost]
-		public ActionResult Delete(int id, FormCollection collection)
-		{
-			try
-			{
-				// TODO: Add delete logic here
-
-				return RedirectToAction("Index");
-			}
-			catch
-			{
-				return View();
-			}
 		}
 	}
 }
