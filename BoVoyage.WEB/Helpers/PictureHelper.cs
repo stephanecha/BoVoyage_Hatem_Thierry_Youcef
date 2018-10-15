66837993c355e76810adf14f494b161e0a9643de
YoosZ <so_yoos@msn.com>
2018-10-15 14:49:56 +0200

    Photo grande

--- BoVoyage.WEB/Helpers/PictureHelper.cs
+++ BoVoyage.WEB/Helpers/PictureHelper.cs
@@ -1,28 +1,25 @@
 ﻿using BoVoyage.DAL.Entites;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BoVoyage.WEB.Helpers
 {
-    public static class PictureHelper
-    {
-        public static MvcHtmlString PictureTournament(this HtmlHelper helper, DestinationPicture picture, string cssClass = "")
-        {
-            var image = new TagBuilder("img");
+	public static class PictureHelper
+	{
+		public static MvcHtmlString PictureTournament(this HtmlHelper helper, DestinationPicture picture, string cssClass = "", string style = "width:100px")
+		{
+			var image = new TagBuilder("img");
 
-            var base64 = Convert.ToBase64String(picture.Content);
-            var src = $"data:{picture.ContentType};base64,{base64}";
+			var base64 = Convert.ToBase64String(picture.Content);
+			var src = $"data:{picture.ContentType};base64,{base64}";
 
-            image.Attributes.Add("src", src);
-            image.Attributes.Add("alt", picture.Nom);
-            image.Attributes.Add("style","width:100px");
-            if (!string.IsNullOrWhiteSpace(cssClass))
-                image.Attributes.Add("class", cssClass);
+			image.Attributes.Add("src", src);
+			image.Attributes.Add("alt", picture.Nom);
+			image.Attributes.Add("style", style);
+			if (!string.IsNullOrWhiteSpace(cssClass))
+				image.Attributes.Add("class", cssClass);
 
-            return MvcHtmlString.Create(image.ToString());
-        }
-    }
+			return MvcHtmlString.Create(image.ToString());
+		}
+	}
 }
